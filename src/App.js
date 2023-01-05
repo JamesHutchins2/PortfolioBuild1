ba0ef974fa92c0740d9d77981b1303b91d34a58c
james <jamesy77@outlook.com>
2023-01-05 16:28:33 -0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './index.css'
-import StockApi from './components/StockData/StockApi'
+
 import Header from './components/Header/Header'
 import Navbar from './components/navbar/navbar'
 import About from './components/about/About'
 import Contact from './components/contact/Contact'
 import Experience from './components/experience/Experience'
 import Footer from './components/Footer/Footer'
-import ProjectMain from './components/Projects/ProjectMain'
+
 
 import './index.css'
 
