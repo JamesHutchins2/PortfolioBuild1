92f30524d33b8bddbb2bb813ab09bcc95bcde8e5
james <jamesy77@outlook.com>
2023-01-12 21:17:17 -0500

    update of SEC project

--- src/components/SEC/SEC.jsx
+++ src/components/SEC/SEC.jsx
@@ -19,10 +19,30 @@ const SEC =  () => {
                 </div>
                 <div className='image'>
                     <img src={diagram} alt="logo" className='diag' />
+
+                </div>
+
+
                 </div>
                     
                 </div>
-            </div>
+                <div className='data'>
+                <h1 className='hd'>Mass Data</h1>
+                <br />
+                <br />
+                    <div>
+                        <p className='h'>
+                            The data set for this project constists of around 6,000,000 XML files all with slight changes, espeicaly for which came before 2003. This has posed some trouble, hopwever progeress has been made. If it becomes required I am currently
+                            developing my skills with the NLTK (Natrual Language Tool Kit) library in python. This library is used for natural language processing, and I believe it will be useful in the future for this project if so required. I hope to soon start to implement the stock data portion of this project which will allow me to start training models.
+
+                        </p>
+                        <br />
+                        <br />
+                        <a className='btnf'href="https://github.com/JamesHutchins2/SEC-Form4Model/tree/main/exploritory/FileData/parsingData">Repository</a>
+                    </div>
+                </div>
+                
+            
         </div>
     )
 }
